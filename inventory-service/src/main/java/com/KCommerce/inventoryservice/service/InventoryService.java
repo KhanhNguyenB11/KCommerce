444b39b070455b46aa475b4c7b89fec38f2c3f4a
KhanhNguyenB11 <dantedmc1134@gmail.com>
2024-04-13 20:26:23 +0700

    inter communication and discovery server

--- inventory-service/src/main/java/com/KCommerce/inventoryservice/service/InventoryService.java
+++ inventory-service/src/main/java/com/KCommerce/inventoryservice/service/InventoryService.java
@@ -1,16 +1,24 @@
 package com.KCommerce.inventoryservice.service;
 
+import com.KCommerce.inventoryservice.dto.InventoryResponse;
 import com.KCommerce.inventoryservice.repository.InventoryRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class InventoryService {
     private final InventoryRepository inventoryRepository;
     @Transactional
-    public boolean isInStock(String skuCode){
-        return inventoryRepository.findBySkuCode(skuCode).isPresent();
+    public List<InventoryResponse> isInStock(List<String> skuCode){
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream().map(inventory ->
+            InventoryResponse.builder()
+                    .skuCode(inventory.getSkuCode())
+                    .isInStock(inventory.getQuantity() > 0)
+                    .build()
+        ).toList();
     }
 }
--- order-service/src/main/java/com/KCommerce/orderservice/service/OrderService.java
+++ order-service/src/main/java/com/KCommerce/orderservice/service/OrderService.java
@@ -1,5 +1,6 @@
 package com.KCommerce.orderservice.service;
 
+import com.KCommerce.orderservice.dto.InventoryResponse;
 import com.KCommerce.orderservice.dto.OrderLineItemsDto;
 import com.KCommerce.orderservice.dto.OrderRequest;
 import com.KCommerce.orderservice.model.Order;
@@ -8,7 +9,9 @@ import com.KCommerce.orderservice.repository.OrderRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 
@@ -17,6 +20,7 @@ import java.util.UUID;
 @Transactional
 public class OrderService {
     private final OrderRepository orderRepository;
+    private final WebClient.Builder webClient;
     public void placeOrder(OrderRequest orderRequest){
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
@@ -25,7 +29,21 @@ public class OrderService {
                 .map(this::mapToDto)
                 .toList();
         order.setOrderLineItemsList(orderLineItemsList);
-        orderRepository.save(order);
+        List<String> skuCodes = order.getOrderLineItemsList().stream().map(OrderLineItems::getSkuCode).toList();
+        //Call inventory service and place order if product is in stock
+        InventoryResponse[] inventoryResponsesArray = webClient.build().get()
+                .uri("http://inventory-service/api/inventory",
+                        uriBuilder -> uriBuilder.queryParam("skuCode",skuCodes).build())
+                .retrieve()
+                        .bodyToMono(InventoryResponse[].class)
+                                .block();
+        boolean allProductInStock = Arrays.stream(inventoryResponsesArray).allMatch(InventoryResponse::isInStock);
+        if(allProductInStock){
+            orderRepository.save(order);
+        }
+        else{
+            throw new IllegalArgumentException("Product is out of stock");
+        }
     }
     public OrderLineItems mapToDto (OrderLineItemsDto orderLineItemsDto){
         OrderLineItems orderLineItems = new OrderLineItems();
